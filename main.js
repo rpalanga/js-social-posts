c1c2eef2b20bd80b1139c42ed778a6990893c9c6
roberto-palanga <r.palanga94@gmail.com>
2024-02-15 16:05:22 +0100

    Milestone 1 e seconda completata

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+/*
+
+Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+id del post, numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Non è necessario creare date casuali Per le immagini va bene utilizzare qualsiasi
+ servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>) V
+
+Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html,
+ stampiamo i post del nostro feed. 
+
+Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone
+ e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post
+  ai quali abbiamo messo il like.
+
+*/
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +77,51 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+ const postList = document.getElementById("container");
+
+ posts.forEach(function(postElement){
+
+    console.log(postElement)
+
+    postList.innerHTML+=`
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${postElement.author.image}" alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${postElement.author.name}</div>
+                <div class="post-meta__time">${postElement.created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+    <div class="post__image">
+        <img src="${postElement.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${postElement.likes}</b> persone
+            </div>
+        </div> 
+    </div>    
+    
+    
+    `;
+
+
+
+
+
+ });
+
